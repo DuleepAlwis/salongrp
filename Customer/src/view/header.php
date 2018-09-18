3fca44f49e9df626f3f23421d1ce5c18e4c83011
JH Jayawardhana <j.hashan.j@gmail.com>
2018-09-18 14:12:51 +0530

    Customer module
    This is separate folder so no conflict after push to master

---
+++ Customer/src/view/header.php
@@ -0,0 +1,95 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <title>Salon Sanrooka</title>
+  <link rel="stylesheet" href="../css/font-awesome.min.css">
+  <link rel="stylesheet" href="../css/bootstrap.css">
+  <link rel="stylesheet" href="../css/style.css">
+</head>
+<body>
+  <nav class="navbar navbar-expand-sm navbar-dark bg-dark p-0">
+    <div class="container">
+      <a href="" class="navbar-brand">Salon Sanrooka</a> 
+       <button class="navbar-toggler" data-toggle="collapse" data-target="#navbarNav">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <!-- <button class="navbar-toggler" data-toggle="collapse" data-target="#navbarNav">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarNav">
+        <ul class="navbar-nav">
+          <li class="nav-item px-2">
+            <a href="CustomerIndex.html" class="nav-link active">New Appoinment</a>
+          </li>
+          <li class="nav-item px-2">
+            <a href="CustomerViewApp.html" class="nav-link">View Appoinments</a>
+          </li> -->
+          <!-- <li class="nav-item px-2">
+            <a href="categories.html" class="nav-link">Categories</a>
+          </li>
+          <li class="nav-item px-2">
+            <a href="users.html" class="nav-link">Users</a>
+          </li> -->
+       <!--  </ul> -->
+
+        <ul class="navbar-nav ml-auto">
+          <li class="nav-item dropdown mr-3">
+            <a href="#" class="nav-link dropdown-toggle" data-toggle="dropdown">
+              <i class="fa fa-user"></i>
+            </a>
+            <div class="dropdown-menu">
+              <a href="CustomerProfile.html" class="dropdown-item">
+                <i class="fa fa-user-circle"></i> Profile
+              </a>
+              <a href="CustomerLogin.html" class="dropdown-item">
+                <i class="fa fa-gear"></i>Logout
+              </a>
+            </div>
+          </li>
+          <!-- <li class="nav-item">
+            <a href="CustomerLogin.html" class="nav-link">
+              <i class="fa fa-user-times"></i> Logout
+            </a>
+          </li> -->
+        </ul>
+      </div>
+    </div>
+  </nav>
+
+  <header id="main-header" class="py-2 bg-primary text-white">
+   <!--  <div class="container">
+      <div class="row">
+        <div class="col-md-6">
+          <h1><i class="fa fa-gear"></i>New Appoinments</h1>
+        </div>
+      </div>
+    </div> -->
+    <nav class="navbar navbar-expand-sm navbar-dark bg-dark p-0">
+    <div class="container">
+      <!-- <a href="CustomerIndex.html" class="navbar-brand">Blogen</a> -->
+     <!--  <label>Salon Sanrooka</label> -->
+      <button class="navbar-toggler" data-toggle="collapse" data-target="#navbarNav">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarNav">
+        <ul class="navbar-nav">
+          <li class="nav-item px-2">
+            <a href="CustomerIndex.php"  name="makeAppoinment" class="nav-link">Make Appoinment</a>
+          </li>
+          <li class="nav-item px-2">
+            <a href="CustomerViewApp.php"  name="viewAppoinment" class="nav-link">View Appoinments</a>
+          </li>
+          <!-- <li class="nav-item px-2">
+            <a href="categories.html" class="nav-link">Categories</a>
+          </li>
+          <li class="nav-item px-2">
+            <a href="users.html" class="nav-link">Users</a>
+          </li> -->
+        </ul>
+        </div>
+    </div>
+  </nav>
+  </header>
